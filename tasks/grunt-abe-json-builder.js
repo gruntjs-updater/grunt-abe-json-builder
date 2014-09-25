a4f23e47961498cdadc245d04d682fe976e54068
Nick White <nick.white@rockabox.com>
2014-09-25 17:28:16 +0100

    Create Grunt task for abe-json-builder
    - Warns user if they are not passing:
      - build (location to put new JSON files)
      - location (where current ABE JSON files live)

---
+++ tasks/grunt-abe-json-builder.js
@@ -0,0 +1,20 @@
+var lodash = require('lodash-node'),
+    jsonBuilder = require('abe-json-builder');
+
+module.exports = function (grunt) {
+    grunt.task.registerMultiTask('abe-json-builder', function () {
+        // Warn task if "location" and/or "build" is not passed
+        var opt = this.options();
+
+        if (lodash.isEmpty(opt.location)) {
+            grunt.fail.warn('location option is required.');
+        }
+
+        if (lodash.isEmpty(opt.build)) {
+            grunt.fail.warn('build option is required.');
+        }
+
+        jsonBuilder.jsonBuilder(opt);
+        grunt.log.writeln('ABE JSON files body responses have been split');
+    });
+};
